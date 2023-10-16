a3dd8048c251355a33a421c00994d0bad01ba84c
Oksana Ozturk <oksanaozturk@gmail.com>
2023-10-16 23:06:17 +0300

    Создание функцмм converts_all_letters_uppercase

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def converts_to_capital_letters(text):
     new_text = text.title()
     return new_text
+
+
+def converts_all_letters_uppercase(text):
+    """Функцмя, выводящая все слова в верхнем регистре"""
+    new_text_1 = text.upper()
+    return new_text_1
